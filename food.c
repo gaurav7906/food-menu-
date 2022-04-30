ca25bf204683310eca354f21ab99e06f674ce06e
gaurav7906 <104683487+gaurav7906@users.noreply.github.com>
2022-04-30 20:08:28 +0530

    Add files via upload

---
+++ food.c
@@ -0,0 +1,19 @@
+void main()
+{
+ int x ;
+ printf("enter number between one to five for order");
+ scanf("%d",&x);
+  switch (x) 
+  { 
+  case 1: printf(" \nfood item- idli\n price- 100");
+   break;
+  case 2 : printf("\n food item- momos\n price-60")
+ break;
+ case 3: printf("\n food item-pizza\n price-250")
+	 break;
+case 4: printf("\n food item-pasta\n price-150")
+	 break;
+case 5 : printf("\n food item-sandwich\n price-80")
+	 break;
+}
+}
